1b1dbafad23d733c0c0aac5eb77a5057643e93d6
Priscila <pri.wagner.silva@gmail.com>
2025-03-14 14:27:58 +0000

    added style

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+
+// Compile and minify SCSS
+function styles() {
+    return gulp.src('./src/styles/**/*.scss') // Use '**/*.scss' to include subdirectories
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'));
+}
+
+// Optimize images
+function images() {
+    return gulp.src('./src/images/**/*', { encoding: false })
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+// Watch for changes in SCSS and images
+function watchFiles() {
+    gulp.watch('./src/styles/**/*.scss', styles);
+    gulp.watch('./src/images/**/*', images); // Watch for image changes
+}
+
+// Default task runs both styles and images
+exports.default = gulp.parallel(styles, images, watchFiles);
+exports.watch = watchFiles;
+exports.styles = styles;
+exports.images = images;
